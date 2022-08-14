7e4060a6fcb2790a288271d96d381716d7efe3b6
sherryxiaoyizhu <zhuxy9474@gmail.com>
2022-08-14 12:52:33 -0500

    Auth Reducer & Register Action

--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -2,8 +2,9 @@ import { SET_ALERT, REMOVE_ALERT } from '../actions/types';
 
 const initialState = [];
 
-export default function (state = initialState, action) {
+function alertReducer(state = initialState, action) {
   const { type, payload } = action;
+
   switch (type) {
     case SET_ALERT:
       return [...state, payload];
@@ -13,3 +14,5 @@ export default function (state = initialState, action) {
       return state;
   }
 }
+
+export default alertReducer;
